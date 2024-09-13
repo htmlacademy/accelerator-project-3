a2502cf74f65bf9d2d0f648d06d13fc44aed6067
restby <restby@gmail.com>
2024-09-13 11:11:05 +0300

    not fix tabIndex

--- source/js/modules/burger-menu.js
+++ source/js/modules/burger-menu.js
@@ -1,30 +1,31 @@
 const initBurgerMenu = () => {
   const burgerButton = document.querySelector('[data-element="nav__button"]');
   const menu = document.querySelector('[data-element="menu"]');
-  const body = document.body;
-  const overlay = document.querySelector('.overlay');
+  // const body = document.body;
+  // const overlay = document.querySelector('.overlay');
+
+  // const setMenuTabIndex = (index) => {
+  //   const menuLinks = menu.querySelectorAll('a');
+  //   menuLinks.forEach((link) => {
+  //     link.tabIndex = index;
+  //   });
+  // };
 
   const toggleMenu = () => {
-    const isOpen = burgerButton.classList.toggle('is-open');
+    burgerButton.classList.toggle('is-open');
+    // const isOpen = burgerButton.classList.toggle('is-open');
     menu.classList.toggle('is-open');
-    body.classList.toggle('no-scroll');
-    overlay.classList.toggle('is-visible');
-    setMenuTabIndex(isOpen ? 0 : -1);
+    // body.classList.toggle('no-scroll');
+    // overlay.classList.toggle('is-visible');
+    // setMenuTabIndex(isOpen ? 0 : -1);
   };
 
   const closeMenu = () => {
     burgerButton.classList.remove('is-open');
     menu.classList.remove('is-open');
-    body.classList.remove('no-scroll');
-    overlay.classList.remove('is-visible');
-    setMenuTabIndex(-1);
-  };
-
-  const setMenuTabIndex = (index) => {
-    const menuLinks = menu.querySelectorAll('a');
-    menuLinks.forEach(link => {
-      link.tabIndex = index;
-    });
+    // body.classList.remove('no-scroll');
+    // overlay.classList.remove('is-visible');
+    // setMenuTabIndex(-1);
   };
 
   burgerButton.addEventListener('click', () => {
@@ -36,16 +37,17 @@ const initBurgerMenu = () => {
     const submenu = target.nextElementSibling;
 
     if (target.tagName === 'A' && submenu && submenu.classList.contains('nav__submenu-list')) {
-      const isOpen = target.classList.toggle('is-open');
+      target.classList.toggle('is-open');
+      // const isOpen = target.classList.toggle('is-open');
       event.preventDefault();
       submenu.classList.toggle('is-open');
-      setMenuTabIndex(isOpen ? 0 : -1);
+      // setMenuTabIndex(isOpen ? 0 : -1);
     } else if (target.tagName === 'A') {
       closeMenu();
     }
   });
 
-  overlay.addEventListener('click', closeMenu);
+  // overlay.addEventListener('click', closeMenu);
 
   document.addEventListener('keydown', (event) => {
     if (event.key === 'Escape') {
@@ -53,7 +55,7 @@ const initBurgerMenu = () => {
     }
   });
 
-  setMenuTabIndex(-1);
+  // setMenuTabIndex(-1);
 };
 
 export { initBurgerMenu };
