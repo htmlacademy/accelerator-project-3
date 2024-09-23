342af4ae7e6ec3b3aa57f79e2d06069fced009ab
restby <restby@gmail.com>
2024-09-24 00:48:26 +0300

    not

--- source/js/modules/sliders/news-slider.js
+++ source/js/modules/sliders/news-slider.js
@@ -3,197 +3,10 @@ import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-// const initNewsSlider = () => {
-//   const newsSlider = document.querySelector('[data-slider="news-tab-slider"]');
-//   if (!newsSlider) {
-//     return;
-//   }
-
-//   // Клонирование слайдов до необходимого количества
-//   const slides = newsSlider.querySelectorAll('.swiper-slide');
-//   const slidesToClone = 4 * 3; // 4 страницы по 3 слайда
-//   const totalSlides = slides.length;
-
-//   if (totalSlides < slidesToClone) {
-//     for (let i = 0; i < slidesToClone - totalSlides; i++) {
-//       const clone = slides[i % totalSlides].cloneNode(true);
-//       newsSlider.querySelector('.swiper-wrapper').appendChild(clone);
-//     }
-//   }
-
-//   new Swiper(newsSlider, {
-//     modules: [Navigation, Pagination],
-//     navigation: {
-//       nextEl: '.news-tab__slider-btn--next',
-//       prevEl: '.news-tab__slider-btn--prev',
-//       disabledClass: 'is-disabled',
-//     },
-//     pagination: {
-//       el: '.news-tab__slider-pagination',
-//       clickable: true,
-//       bulletElement: 'button',
-//       bulletClass: 'news-tab__slider-bullet',
-//       bulletActiveClass: 'news-tab__slider-bullet--is-active',
-//       renderBullet: function (index, className) {
-//         return '<button class="' + className + '" type="button">' + (index + 1) + '</button>';
-//       },
-//     },
-//     watchOverflow: false,
-//     simulateTouch: false,
-//     grabCursor: false,
-//     speed: 500,
-//     loop: false,
-//     spaceBetween: 32,
-//     slidesPerView: 'auto',
-//     slidesPerGroup: 3,
-//     breakpoints: {
-//       1440: {
-//         allowTouchMove: false,
-//         slidesPerView: 'auto',
-//         slidesPerGroup: 3,
-//         spaceBetween: 32,
-//       }
-//     },
-//     on: {
-//       init: function () {
-//         updatePagination(this);
-//       },
-//       slideChange: function () {
-//         updatePagination(this);
-//       },
-//     },
-//   });
-
-//   function updatePagination(swiper) {
-//     const totalSlides = swiper.slides.length;
-//     const currentIndex = swiper.activeIndex;
-//     const paginationButtons = document.querySelectorAll('.news-tab__slider-bullet');
-//     paginationButtons.forEach((button, index) => {
-//       if (totalSlides <= 4 * 3) {
-//         button.style.display = 'inline-block';
-//       } else {
-//         if (index >= currentIndex / 3 - 1 && index <= currentIndex / 3 + 2) {
-//           button.style.display = 'inline-block';
-//         } else {
-//           button.style.display = 'none';
-//         }
-//       }
-//     });
-//   }
-// };
-
-// export { initNewsSlider };
-
-
-// const initNewsSlider = () => {
-//   const newsSlider = document.querySelector('[data-slider="news-tab-slider"]');
-//   if (!newsSlider) {
-//     return;
-//   }
-
-//   // Клонирование слайдов до необходимого количества
-//   const slides = newsSlider.querySelectorAll('.swiper-slide');
-//   const slidesToClone = 4 * 3; // 4 страницы по 3 слайда
-//   const totalSlides = slides.length;
-
-//   if (totalSlides < slidesToClone) {
-//     for (let i = 0; i < slidesToClone - totalSlides; i++) {
-//       const clone = slides[i % totalSlides].cloneNode(true);
-//       newsSlider.querySelector('.swiper-wrapper').appendChild(clone);
-//     }
-//   }
-
-//   new Swiper(newsSlider, {
-//     modules: [Navigation, Pagination],
-//     navigation: {
-//       nextEl: '.news-tab__slider-btn--next',
-//       prevEl: '.news-tab__slider-btn--prev',
-//       disabledClass: 'is-disabled',
-//     },
-//     pagination: {
-//       el: '.news-tab__slider-pagination',
-//       clickable: true,
-//       bulletElement: 'button',
-//       bulletClass: 'news-tab__slider-bullet',
-//       bulletActiveClass: 'news-tab__slider-bullet--is-active',
-//       renderBullet: function (index, className) {
-//         return '<button class="' + className + '" type="button">' + (index + 1) + '</button>';
-//       },
-//     },
-//     watchOverflow: false,
-//     simulateTouch: false,
-//     grabCursor: false,
-//     speed: 500,
-//     loop: false,
-//     spaceBetween: 32,
-//     slidesPerView: 'auto',
-//     slidesPerGroup: 3,
-//     breakpoints: {
-//       1440: {
-//         allowTouchMove: false,
-//         slidesPerView: 'auto',
-//         slidesPerGroup: 3,
-//         spaceBetween: 32,
-//       }
-//     },
-//     on: {
-//       init: function () {
-//         updatePagination(this);
-//         markFirstSlide(this);
-//       },
-//       slideChange: function () {
-//         updatePagination(this);
-//         markFirstSlide(this);
-//       },
-//     },
-//   });
-
-//   function updatePagination(swiper) {
-//     const totalSlides = swiper.slides.length;
-//     const currentIndex = swiper.activeIndex;
-//     const paginationButtons = document.querySelectorAll('.news-tab__slider-bullet');
-//     paginationButtons.forEach((button, index) => {
-//       if (totalSlides <= 4 * 3) {
-//         button.style.display = 'inline-block';
-//       } else {
-//         if (index >= currentIndex / 3 - 1 && index <= currentIndex / 3 + 2) {
-//           button.style.display = 'inline-block';
-//         } else {
-//           button.style.display = 'none';
-//         }
-//       }
-//     });
-//   }
-
-//   function markFirstSlide(swiper) {
-//     const slides = swiper.slides;
-//     slides.forEach((slide, index) => {
-//       slide.classList.remove('news-slide--main');
-//       if (index % 3 === 0) {
-//         slide.classList.add('news-slide--main');
-//       }
-//     });
-//   }
-// };
-
-// export { initNewsSlider };
-
 const initNewsSlider = () => {
   const newsSlider = document.querySelector('[data-slider="news-tab-slider"]');
   if (!newsSlider) {
     return;
-  }
-
-  // Клонирование слайдов до необходимого количества
-  const slides = newsSlider.querySelectorAll('.swiper-slide');
-  const slidesToClone = 4 * 3; // 4 страницы по 3 слайда
-  const totalSlides = slides.length;
-
-  if (totalSlides < slidesToClone) {
-    for (let i = 0; i < slidesToClone - totalSlides; i++) {
-      const clone = slides[i % totalSlides].cloneNode(true);
-      newsSlider.querySelector('.swiper-wrapper').appendChild(clone);
-    }
   }
 
   new Swiper(newsSlider, {
@@ -213,17 +26,26 @@ const initNewsSlider = () => {
         return '<button class="' + className + '" type="button">' + (index + 1) + '</button>';
       },
     },
-    watchOverflow: false,
-    simulateTouch: false,
-    grabCursor: false,
+    watchOverflow: true,
+    simulateTouch: true,
+    grabCursor: true,
     speed: 500,
     loop: false,
-    spaceBetween: 32,
-    slidesPerView: 'auto',
-    slidesPerGroup: 3,
     breakpoints: {
+      320: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 0,
+      },
+      768: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 0,
+        grid: {
+          rows: 2,
+        },
+      },
       1440: {
-        allowTouchMove: false,
         slidesPerView: 'auto',
         slidesPerGroup: 3,
         spaceBetween: 32,
